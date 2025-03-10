fbe69957b76c576b5012f90a4ae582851748ee7b
Hasan Atki <hasanatki@live.com>
2025-03-10 14:22:56 +0000

    add model form for creating accounts

--- bankhome/models.py
+++ bankhome/models.py
@@ -1,18 +1,30 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 
 # Create your models here.
 
-class CustomUser(User):
+# class CustomUser(User):
 
-    def create_user(self, email, first_name, last_name, address, balance, password):
-        if not email:
-            raise ValueError("You have not provided a valid email address.")
+#     def create_user(self, email, first_name, last_name, address, balance, password):
+#         if not email:
+#             raise ValueError("You have not provided a valid email address.")
     
-        email = self.normalize_email(email)
-        first_name = self.first_name
-        last_name = self.last_name
-        address = self.address
-        balance = self.balance
-        password = self.set_password(password)
+#         email = self.normalize_email(email)
+#         first_name = self.first_name
+#         last_name = self.last_name
+#         address = self.address
+#         balance = self.balance
+#         password = self.set_password(password)
         
+
+class Account(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    id_number = models.IntegerField(validators=[MaxValueValidator(999)])
+    balance = models.IntegerField(validators=[MaxValueValidator(999999)])
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- bankhome/views.py
+++ bankhome/views.py
@@ -1,37 +1,41 @@
 from django.shortcuts import render
+from .models import Account
+from .forms import AccountForm
 
 def login(request):
     return render(request, "login/login.html")
 
 def signup(request):
-    return render(request, "signup/signup.html")
+    form = AccountForm()
+    context = {"form": form}
+    return render(request, "signup/signup.html", context)
 
-objs = [{
-        "id": 1,
-        "first_name": "John",
-        "last_name": "Sexton",
-        "address": "12 Main Street",
-        "balance": 8300
-    },
-    {
-        "id": 2,
-        "first_name": "Mary",
-        "last_name": "Sexton",
-        "address": "12 Main Street",
-        "balance": 8300
-    },
-    {
-        "id": 3,
-        "first_name": "Jane",
-        "last_name": "Sexton",
-        "address": "12 Main Street",
-        "balance": 8300
-    }
-]
+# objs = [{
+#         "id": 1,
+#         "first_name": "John",
+#         "last_name": "Sexton",
+#         "address": "12 Main Street",
+#         "balance": 8300
+#     },
+#     {
+#         "id": 2,
+#         "first_name": "Mary",
+#         "last_name": "Sexton",
+#         "address": "12 Main Street",
+#         "balance": 8300
+#     },
+#     {
+#         "id": 3,
+#         "first_name": "Jane",
+#         "last_name": "Sexton",
+#         "address": "12 Main Street",
+#         "balance": 8300
+#     }
+# ]
 
 def dashboard(request):
-    context = {"objs": objs}
-    return render(request, 'dashboard/dashboard.html', context)
+    objs = Account.objects
+    return render(request, 'dashboard/dashboard.html', {"objs": objs})
 
 def read(request, pk):
     obj = None
