93524001cb6e0f699ca9ce845f411fd605b23c89
Hasan Atki <hasanatki@live.com>
2025-03-10 12:54:34 +0000

    update urls to redirect

--- bankhome/urls.py
+++ bankhome/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name="login"),
+    path('', views.login, name="login"),
     path('signup', views.signup, name="signup"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('read/<str:pk>', views.read, name='read')
--- bankhome/views.py
+++ bankhome/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 def login(request):
     return render(request, "login/login.html")
