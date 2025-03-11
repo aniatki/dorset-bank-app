654110e5856901c492e904d0d638f401d6e5d8c1
Hasan Atki <hasanatki@live.com>
2025-03-11 20:34:33 +0000

    Authenticate custom User model

--- bankhome/forms.py
+++ bankhome/forms.py
@@ -1,7 +1,16 @@
 from django.forms import ModelForm
-from .models import Account
+from .models import Account, User
+from django import forms
 
 class AccountForm(ModelForm):
     class Meta:
         model = Account
         fields = "__all__"
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = "__all__"
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
--- bankhome/models.py
+++ bankhome/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
  
@@ -14,3 +15,49 @@ class Account(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        """Creates and returns a regular user."""
+        if not username:
+            raise ValueError("The Username field is required")
+        
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None):
+        """Creates and returns a superuser."""
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+    def get_by_natural_key(self, username):
+        """Required for Django authentication to work correctly."""
+        return self.get(username=username)
+
+class User(AbstractBaseUser):
+    username = models.CharField(db_index=True, unique=True, max_length=50)
+    password = models.CharField(max_length=100)
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.username
+    
+    def has_perm(self, perm, obj=None):
+        """Return True if user has a specific permission."""
+        return True 
+
+    def has_module_perms(self, app_label):
+        """Return True if user has permissions for an app."""
+        return True 
